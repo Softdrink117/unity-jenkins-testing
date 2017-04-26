2574173015897904afb086fbc83238f7435f1a01
Tobias Heinemann <tobyhein@gmail.com>
2017-04-25 22:21:22 -0700

    Modified error return in BuildScript

--- Assets/BuildScript.cs
+++ Assets/BuildScript.cs
@@ -73,7 +73,7 @@ public class BuildScript{
 	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options){
 		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
 		string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
-		if (res.Length <= 0) {
+		if (res.Length > 0) {
 			throw new Exception("BuildPlayer failure: " + res);
 		}
 	}
